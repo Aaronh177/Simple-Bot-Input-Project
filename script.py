97bc9f26d2399a810e8bf1b64efa5e10efce890c
Aaron Howarth <aaronhowarth1@hotmail.co.uk>
2025-09-15 13:31:29 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,9 @@
+name = input("Hello! what is your name?")
+print(f"Nice to meet you, {name}!")
+age_input = input("How old are you?")
+age = int(age_input)
+bot_age = 15
+age_difference = age - bot_age
+print(f"you are {age_difference} years older than me. I'm only {bot_age} years old!")
+color = input("What's your favorite color?")
+print(f"Oh, {color} is an amazing color!")
